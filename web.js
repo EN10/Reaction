f68f33298dc9a280c0e40ac161b812666697fea2
EN10 <sfm.neo@gmail.com>
2013-12-11 23:16:12 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,18 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-<<<<<<< HEAD
   response.send('Hello World!');
-=======
-  response.send('Hello World!2');
->>>>>>> 221220f7e403d8369999fa0dca806b4b8e7ed31d
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 221220f7e403d8369999fa0dca806b4b8e7ed31d
