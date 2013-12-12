37eb67553aeeb1507b6c2a61797329ef04493240
EN10 <sfm.neo@gmail.com>
2013-12-12 15:58:03 +0000

    webjs & folder

--- web.js
+++ web.js
@@ -5,13 +5,11 @@ var app = express();
 app.use(express.logger());
 
 var fs = require('fs');
-var filename = '/html5/index.html';
-var index = fs.readFileSync(filename);
-var buffer = new Buffer(index, "utf-8");
-
+var filename = 'index.html';
+var index = fs.readFileSync(filename, 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
