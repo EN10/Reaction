7467b93b73575f43d972d5eab31efea08dc230c4
EN10 <sfm.neo@gmail.com>
2013-12-12 10:28:21 +0000

    Update web.js
    Output index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var filename = '/html5/index.html'
+var index = fs.readFileSync(filename, String)
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
