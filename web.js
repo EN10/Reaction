81f264653cfa706f44522e2f33b943de17abe81c
EN10 <sfm.neo@gmail.com>
2013-12-14 18:02:20 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@
 
  /* serves main page */
  app.get("./", function(req, res) {
-    res.sendfile('index.html')
+    res.sendfile('html5/index.html')
  });
 
   app.post("/user/add", function(req, res) { 
@@ -14,7 +14,7 @@
  /* serves all the static files */
  app.get(/^(.+)$/, function(req, res){ 
      console.log('static file request : ' + req.params);
-     res.sendfile(__dirname + '/html5' + req.params[0]); 
+     res.sendfile(__dirname + req.params[0]); 
  });
 
  var port = process.env.PORT || 80;
