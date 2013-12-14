45d45ce38a1829ce8aae51c9fbb2aaded843663c
EN10 <sfm.neo@gmail.com>
2013-12-14 12:30:21 +0000

    html5 folder

--- web.js
+++ web.js
@@ -3,7 +3,7 @@
 
  /* serves main page */
  app.get("/", function(req, res) {
-    res.sendfile('index.html')
+    res.sendfile('html5/index.html')
  });
 
   app.post("/user/add", function(req, res) { 
